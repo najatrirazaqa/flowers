3c5ca433ede8ae2ed92d80e8c62fc6228acfffa7
najatrirazaqa <trirazaqa@gmail.com>
2022-12-12 18:29:50 +0700

    update v1

--- main.js
+++ main.js
@@ -1,3 +1,48 @@
-onload = () =>{
-        document.body.classList.remove("container");
+onload = () => {
+  document.body.classList.remove("container");
 };
+
+// Code View Source Code
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+
+document.onkeypress = function (event) {
+  event = event || window.event;
+  if (event.keyCode == 123) {
+    return false;
+  }
+};
+document.onmousedown = function (event) {
+  event = event || window.event;
+  if (event.keyCode == 123) {
+    return false;
+  }
+};
+document.onkeydown = function (event) {
+  event = event || window.event;
+  if (event.keyCode == 123) {
+    return false;
+  }
+};
+jQuery(document).ready(function ($) {
+  $(document).keydown(function (event) {
+    var pressedKey = String.fromCharCode(event.keyCode).toLowerCase();
+
+    if (event.ctrlKey && (pressedKey == "c" || pressedKey == "u")) {
+      alert("Swiper, jangan mencuri!");
+      //disable key press porcessing
+      return false;
+    }
+  });
+});
+
+jQuery(document).ready(function ($) {
+  $(document).keydown(function (event) {
+    var pressedKey = String.fromCharCode(event.keyCode).toLowerCase();
+
+    if (event.ctrlKey && (pressedKey == "c" || pressedKey == "i")) {
+      alert("Swiper, jangan mencuri!");
+      //disable key press porcessing
+      return false;
+    }
+  });
+});
